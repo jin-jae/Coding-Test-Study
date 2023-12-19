3cf0d5ddd1003c3e45402e846b6387fe46a96beb
jin-jae <jinjae.dev@gmail.com>
2023-12-19 14:28:59 +0900

    add: Solve week01 problem PGM_incomplete_runner

--- jinjae-kim/week01_hash/PGM_incomplete_runner.py
+++ jinjae-kim/week01_hash/PGM_incomplete_runner.py
@@ -1,3 +1,11 @@
 def solution(participant, completion):
-    answer = ''
-    return answer
+    participant_hash = {}
+    for p in participant:
+        if p not in participant_hash:
+            participant_hash[p] = 1 
+        else:
+            participant_hash[p] += 1
+    for completed in completion:
+        participant_hash[completed] -= 1
+    answer = [k for k, v in participant_hash.items() if v == 1]
+    return answer[0]
