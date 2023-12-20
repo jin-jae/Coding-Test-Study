c4a14e6040e5b5c0f29e1100e6a737066b82854e
jin-jae <jinjae.dev@gmail.com>
2023-12-20 22:57:01 +0900

    add: Solve week01 problem PGM_phoneketmon

--- jinjae-kim/week01_hash/PGM_phoneketmon.py
+++ jinjae-kim/week01_hash/PGM_phoneketmon.py
@@ -1,3 +1,8 @@
 def solution(nums):
-    answer = 0
-    return answer
+    nums_dict = {}
+    for num in nums:
+        if num not in nums_dict:
+            nums_dict[num] = 0
+        else:
+            nums_dict[num] += 1
+    return min(len(nums_dict), len(nums) / 2)
