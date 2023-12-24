da94bbca45cccc2195706ba9ce682f74e4582df8
마민정 (MinJeong Ma) <75362328+maj34@users.noreply.github.com>
2023-12-24 20:03:35 +0900

    Update PGM_clothes.py

--- minjeong-ma/week01_hash/PGM_clothes.py
+++ minjeong-ma/week01_hash/PGM_clothes.py
@@ -1,3 +1,13 @@
 def solution(clothes):
-    answer = 0
-    return answer
+    answer = 1
+    dic = {}
+    
+    for i in clothes:
+        if i[1] in dic:
+            dic[i[1]] += 1
+        else:
+            dic[i[1]] = 1
+
+    for i in dic.values():
+        answer *= (i+1)
+    return answer - 1
