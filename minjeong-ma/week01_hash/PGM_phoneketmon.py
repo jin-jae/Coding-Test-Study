1a1524dee66669bee9c050f1536f7417f67ecfac
마민정 (MinJeong Ma) <75362328+maj34@users.noreply.github.com>
2023-12-24 20:04:50 +0900

    Update PGM_phoneketmon.py

--- minjeong-ma/week01_hash/PGM_phoneketmon.py
+++ minjeong-ma/week01_hash/PGM_phoneketmon.py
@@ -1,3 +1,10 @@
 def solution(nums):
     answer = 0
+    n = len(nums) // 2
+    nums = set(nums)
+    
+    if len(nums) > n:
+        answer = n
+    else:
+        answer = len(nums)
     return answer
