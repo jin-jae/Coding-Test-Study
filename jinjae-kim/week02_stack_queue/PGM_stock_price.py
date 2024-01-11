cb8cf4786440a16df418b0fe5da21e5181d9e754
jin-jae <jinjae.dev@gmail.com>
2024-01-11 22:51:27 +0900

    add: Solve week02 problem PGM_stock_price

--- jinjae-kim/week02_stack_queue/PGM_stock_price.py
+++ jinjae-kim/week02_stack_queue/PGM_stock_price.py
@@ -1,3 +1,18 @@
 def solution(prices):
-    answer = []
+    answer = [0] * len(prices)
+    st = [[0, 0, 0]]
+    for i, p in enumerate(prices):
+        # print("stack:", st)
+        while st[-1][1] > p:
+            tmp = st.pop(-1)
+            # print("poped:", tmp)
+            answer[tmp[0]] = tmp[2]
+        st.append([i, p, 0])
+        for s in st:
+            if i != len(prices) - 1:
+                s[2] += 1
+    st.pop(0)
+    for s in st:
+        answer[s[0]] = s[2]
+    # print(st)
     return answer
