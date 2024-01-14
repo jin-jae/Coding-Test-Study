5871fc5d227c805d03b4ae26bd176e4b1c44f138
jin-jae <jinjae.dev@gmail.com>
2024-01-14 13:01:39 +0900

    add: Solve week03 problem PGM_biggest_number

--- jinjae-kim/week03_sort/PGM_biggest_number.py
+++ jinjae-kim/week03_sort/PGM_biggest_number.py
@@ -1,3 +1,8 @@
 def solution(numbers):
-    answer = ''
+    numbers.sort(key=lambda x: str(x) * 3, reverse=True)
+    answer = ""
+    if numbers[0] == 0:
+        return "0"
+    for n in numbers:
+        answer += str(n)
     return answer
