ba021d163f342d1c1f38ae3075e65baf992e030e
jin-jae <jinjae.dev@gmail.com>
2024-01-17 23:27:51 +0900

    add: Solve week03 problem PGM_h_index

--- jinjae-kim/week03_sort/PGM_h_index.py
+++ jinjae-kim/week03_sort/PGM_h_index.py
@@ -1,3 +1,15 @@
+def chk_index(num, array):
+    ret = 0
+    for a in array:
+        if a >= num:
+            ret += 1
+    return ret
+
 def solution(citations):
-    answer = 0
-    return answer
+    citations.sort()
+    answer = [0]
+    for i in range(1, len(citations) + 1):
+        if (chk_index(i, citations) >= i):
+            answer.append(i)
+    print(answer)
+    return answer[-1]
